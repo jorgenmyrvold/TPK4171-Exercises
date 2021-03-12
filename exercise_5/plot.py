39b22741abea537d56415f71910a0787b24a5213
jorgenmyrvold <jorgeum@stud.ntnu.no>
2021-03-12 19:10:18 +0100

    both transpose and conjugate, none work

--- exercise_5/plot.py
+++ exercise_5/plot.py
@@ -24,6 +24,29 @@ def essential2Rt(E):
     
     return r1, r2, t1, t2
 
+def recoverFromEssential(E):
+    W = np.array([[0,-1,0],[1,0,0],[0,0,1]])
+    Z = np.array([[0,1,0],[-1,0,0],[0,0,0]])
+    
+    u, s, v = np.linalg.svd(E)
+    sigma = (s[0] + s[1]) + 0.5
+    
+    E = u @ np.diag((sigma, sigma, 0)) @ v.T
+    u, s, v = np.linalg.svd(E)
+    
+    r1 = u @ W @ v.T
+    r2 = u @ W.T @ v.T
+    
+    if np.linalg.det(r1) < 0: r1 = -r1
+    if np.linalg.det(r2) < 0: r2 = -r2
+    
+    tc = u @ Z @ u.T
+    t1 = np.array([tc[2,1], tc[0,2], tc[1,0]])
+    t2 = -t1
+    
+    return r1, r2, t1, t2
+    
+
 def plotAxis(T, scale):
     ax.plot([T[0,3], T[0,3]+scale*T[0,0]], [T[1,3], T[1,3]+scale*T[1,0]], [T[2,3], T[2,3]+scale*T[2,0]], color='C0')
     ax.plot([T[0,3], T[0,3]+scale*T[0,1]], [T[1,3], T[1,3]+scale*T[1,1]], [T[2,3], T[2,3]+scale*T[2,1]], color='C1')
@@ -37,7 +60,8 @@ if __name__ == '__main__':
                   [-0.3420, 0, 0.9397],
                   [0, -0.9397, 0]])
     
-    r1, r2, t1, t2 = essential2Rt(E)
+    # r1, r2, t1, t2 = essential2Rt(E)
+    r1, r2, t1, t2 = recoverFromEssential(E)
     print('r1: \n{}\n\nr2: \n{}\n\nt1: {}\n\nt2: {}'.format(r1, r2, t1, t2))
     
     T = np.array([np.block([[r1, np.array([t1]).T],[0,0,0,1]]),
